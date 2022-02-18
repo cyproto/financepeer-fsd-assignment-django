23c1cbe6ab0aa3324800449ac683793a2afb69ec
cyproto <kyashkarande@gmail.com>
2022-02-18 05:42:53 +0530

    Make prod ready.

--- financepeer_fsd_assignment/settings.py
+++ financepeer_fsd_assignment/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$e-purkp!a8nfn1xln+t7-g4m4+08a*r160$%ow*7j0ey%x-=b'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -104,8 +109,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'financepeer_assignment',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
+        'USER': env('DB_USER_NAME'),
+        'PASSWORD': env('DB_USER_PASS'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
